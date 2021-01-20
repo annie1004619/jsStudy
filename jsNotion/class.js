c8d0f631dc69eb2a4194f5d83f2a2d8b4c68f199
annie1004619 <annie1004619@naver.com>
2021-01-20 21:28:25 +0900

    [ADD] class

---
+++ jsNotion/class.js
@@ -0,0 +1,14 @@
+// Object-oriented programming
+// class 붕어빵 틀
+// - template
+// - declare once
+// - no data in
+
+// object 팥 붕어빵, 크림 붕어빵..
+// - instance of a class
+// created many times
+// data in
+
+// JavaScript classes
+// - introduced in ES6
+// - syntactical sugar over prototype=based inheritance
